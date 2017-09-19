f6af616452938da654491add20a8bc94c54c1add
chris.nappin <chris@nappin.com>
2017-09-19 13:42:25 +0100

    Updated with latest example, and work around for Packer 1.1.0.

--- basic-standalone-webserver/install_puppet.sh
+++ basic-standalone-webserver/install_puppet.sh
@@ -5,3 +5,11 @@ sudo yum update -y
 
 # install Puppet v3.x
 sudo yum install -y puppet3
+
+# create packer staging directories with write permissions for ec2-user
+# to work around issue 5347 in Packer 1.1.0...
+sudo mkdir /tmp/packer-puppet-masterless
+sudo mkdir /tmp/packer-puppet-masterless/module-0
+sudo mkdir /tmp/packer-puppet-masterless/manifests
+sudo chown -R ec2-user:ec2-user /tmp/packer-puppet-masterless
+sudo chmod -R 775 /tmp/packer-puppet-masterless
